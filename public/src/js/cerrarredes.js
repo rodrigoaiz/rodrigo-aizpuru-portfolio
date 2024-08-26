39184c919c587f9fbe24befb7239c0d48930c75d
rodrigoaiz <rodrigoaiz@gmail.com>
2024-08-26 13:44:47 -0600

    chore: Remove unused files and update UI styles

--- public/src/js/cerrarredes.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  let cerrarRedes = document.getElementById('cerrar-redes');
-  if(cerrarRedes){
-    cerrarRedes.addEventListener('click', function(event){
-      document.getElementById('redes').classList.toggle('quitar-redes');
-      event.preventDefault();
-    });
-  }
-});
