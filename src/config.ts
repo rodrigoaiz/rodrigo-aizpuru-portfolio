f0774a7d52d09bceb04f7bb1ce552f4e870a5d79
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2024-09-24 20:11:19 -0600

    feat: Update site title and description to Spanish; enhance meta tags for SEO

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rodrigo Aizpuru - Frontend Developer & Designer';
-export const SITE_DESCRIPTION = 'Rodrigo Aizpuru is a Frontend Developer & Designer based in Mexico City. He specializes in building websites and applications that are fast, accessible, and easy to use.';
+export const SITE_TITLE = 'Rodrigo Aizpuru - Desarrollador y Diseñador Frontend';
+export const SITE_DESCRIPTION = 'Rodrigo Aizpuru es un Desarrollador Frontend y Diseñador. Se especializa en construir sitios web y aplicaciones que son rápidas, accesibles y fáciles de usar.';
 export const SITE_URL = 'https://rodrigoaizpuru.com';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
 export const IMAGE_OG = 'https://www.rodrigoaizpuru.com/social_img.jpg'
